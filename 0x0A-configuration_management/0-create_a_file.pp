a8d810bc5723ba444a1cc0a1584e2bd09857a77a
bush-da <danielsevani12@gmail.com>
2023-08-30 06:05:48 -0700

    create a file using puppet in /tmp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#create a file
+
+file { 'alx' :
+  ensure  => 'present',
+  path    => '/tmp/school',
+  mode    => '0774',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+  }
