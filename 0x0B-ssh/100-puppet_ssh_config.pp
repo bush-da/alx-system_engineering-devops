fee97ce58dbaab4c8205480a0e067e0411793b75
Daniel Tujuma <danielsevani12@gmail.com>
2024-04-21 16:49:08 -0400

    connect without password config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# connect without password to sever
+file { '/etc/ssh/ssh_config'
+  ensure  => 'present',
+  mode    => '0600',
+  content => "\
+  Host *
+       IdentifyFile ~/.ssh/school
+       PasswordAutentication no
+  ",
+}
