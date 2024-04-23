b217acec862127d6209856093b198e6afcfa0f6e
Daniel Tujuma <danielsevani12@gmail.com>
2024-04-23 09:56:35 -0400

    install flask from pip3 using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
 # install flask from pip3
-
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
+# install werkzeug version 2.1.0
 package { 'werkzeug':
   ensure   => '2.1.0',
   provider => 'pip3',
