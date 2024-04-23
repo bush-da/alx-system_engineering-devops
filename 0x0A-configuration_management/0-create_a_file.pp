a54d1ce1a632727ac49c61e5f3161607a84c38ae
Daniel Tujuma <danielsevani12@gmail.com>
2024-04-23 08:48:56 -0400

    create a file using puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file
+file { '/tmp':
+  ensure  => present,
+  path    => '/tmp/school',
+  mode    => '0744',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
