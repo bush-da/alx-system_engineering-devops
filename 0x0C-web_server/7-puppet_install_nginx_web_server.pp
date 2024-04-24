ba81232c65460a21892eef5b23478cee747f6dd6
Daniel Tujuma <danielsevani12@gmail.com>
2024-04-24 16:10:45 -0400

    config server using puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,53 @@
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/etc/nginx/html':
+  ensure => directory,
+}
+
+file { '/etc/nginx/html/index.html':
+  ensure  => file,
+  content => "Hello World!",
+}
+
+file { '/etc/nginx/html/404.html':
+  ensure  => file,
+  content => "Ceci n'est pas une page",
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => "/
+   server {
+     listen 80;
+     listen [::]:80;
+
+     root /etc/nginx/html;
+     index index.html index.htm;
+
+     server_name _;
+
+     location / {
+         try_files /$uri /$uri\ =404;
+     }
+
+     location /redirect_me {
+       return 301 https://www.youtube.com/watch?v=AfIOBLr1NDU;
+     }
+
+     error_page 404 /404.html;
+
+     location = /404.html {
+       root /etc/nginx/html;
+       internal;
+     }
+}",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+Service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+}
