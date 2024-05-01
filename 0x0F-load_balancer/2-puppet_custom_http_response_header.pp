2df3c1712260a2efbc6b46bdad1ac1fb693984d9
Daniel Tujuma <danielsevani12@gmail.com>
2024-05-01 18:06:28 -0400

    update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -14,27 +14,40 @@ package { 'nginx':
 # Create a new index.html
 file { 'Create index.html':
   require => Package['nginx'],
-  path    => '/var/www/html/index.html',
+  path    => '/etc/nginx/html/index.html',
   content => 'Hello World!\n'
+}
+
+file { '/etc/nginx/html':
+  ensure => directory,
 }
 
 # Create a new error page
 file { 'Create 404.html':
   require => Package['nginx'],
-  path    => '/var/www/html/404.html',
+  path    => '/etc/nginx/html/404.html',
   content => 'Ceci n\'est pas une page\n'
 }
 
 file { '/etc/nginx/sites-available/default':
   content => "server {
-		listen 80 default_server;
+		listen 80;
+                listen [::]:80;
+
 		server_name _;
-		root /var/www/html;
+		root /etc/nginx/html;
 		location / {
 			index index.html;
 			add_header X-Served-By $hostname;
 		}
-		rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+                location /redirect_me {
+                         return 301 https://www.youtube.com/watch?v=AfIOBLr1NDU;
+                }
+                error_page 404 /404.html;
+                location /404.html {
+                         root /etc/nginx/html;
+                         internal;
+                }
 	}",
   require => Package['nginx'],
 }
