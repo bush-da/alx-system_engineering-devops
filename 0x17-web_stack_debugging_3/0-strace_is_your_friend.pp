a1cae920fb51f36fc19ac230b7593a01b4c85bdb
Daniel Tujuma <danielsevani12@gmail.com>
2024-08-18 19:03:04 -0400

    fix typo error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Puppet manifest to fix incorrect file extensions in WordPress configuration
 
-exce { 'fix-wordpress':
+exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin:/bin:/usr/bin'
 }
