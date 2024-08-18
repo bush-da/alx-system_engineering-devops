8ddbcf46bab680212bdccbf96fc3f8c3f331adf4
Daniel Tujuma <danielsevani12@gmail.com>
2024-08-18 18:54:10 -0400

    fix 500 error cause by mistype

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Puppet manifest to fix incorrect file extensions in WordPress configuration
+
+exce { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin:/bin:/usr/bin'
+}
